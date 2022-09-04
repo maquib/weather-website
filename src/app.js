a0497df20c7fc941b5848c00e3c625f50d817da5
Mohammad Aquib <mohammad.aquib@quytech.com>
2022-09-04 19:58:47 +0530

    set up for heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define Path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -106,6 +107,6 @@ app.get('/products', (req,res)=>{
         products: []
     })
 })
-app.listen(3000,()=>{
-    console.log('server is up on port 3000.')
+app.listen(port,()=>{
+    console.log('server is up on port '+port)
 })
